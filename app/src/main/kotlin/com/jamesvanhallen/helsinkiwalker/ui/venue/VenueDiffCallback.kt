8f2fb29f2e8fc045f08dfcca4bad2ace7fa239de
Илья Бублик <ilya.bublik@e-legion.com>
2019-06-12 14:28:17 +0300

    VenueDiffCallback fixed

--- app/src/main/kotlin/com/jamesvanhallen/helsinkiwalker/ui/venue/VenueDiffCallback.kt
+++ app/src/main/kotlin/com/jamesvanhallen/helsinkiwalker/ui/venue/VenueDiffCallback.kt
@@ -3,18 +3,18 @@ package com.jamesvanhallen.helsinkiwalker.ui.venue
 import androidx.recyclerview.widget.DiffUtil
 import com.jamesvanhallen.helsinkiwalker.model.database.venue.Venue
 
-class VenueDiffCallback(var newPersons: List<Venue>, var oldPersons: List<Venue>) :
+class VenueDiffCallback(var newVenues: List<Venue>, var oldVenues: List<Venue>) :
     DiffUtil.Callback() {
 
-    override fun getOldListSize() = oldPersons.size
+    override fun getOldListSize() = oldVenues.size
 
-    override fun getNewListSize() = newPersons.size
+    override fun getNewListSize() = newVenues.size
 
     override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldPersons[oldItemPosition].uuid === newPersons[newItemPosition].uuid
+        return oldVenues[oldItemPosition].uuid === newVenues[newItemPosition].uuid
     }
 
     override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
-        return oldPersons[oldItemPosition] == newPersons[newItemPosition]
+        return oldVenues[oldItemPosition] == newVenues[newItemPosition]
     }
 }
